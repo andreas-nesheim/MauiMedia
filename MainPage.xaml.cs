db18c473cdd080cf5795c16c9e6718bc4ca23829
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-12-21 19:29:04 +0100

    added more buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,18 +1,25 @@
 ﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace MauiMedia;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void OnCounterClicked(object sender, EventArgs e)
-	{
-		var result = await CrossMedia.Current.PickPhotoAsync();
-		var stuff = 1;
-	}
+    private async void OnPickPhotoClicked(object sender, EventArgs e)
+    {
+        var result = await CrossMedia.Current.PickPhotoAsync();
+        var stuff = 1;
+    }
+
+    private async void OnTakePhotoClicked(object sender, EventArgs e)
+    {
+        var result = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions { CompressionQuality = 50 });
+        var stuff = 1;
+    }
 }
 
