b18c67ec094449d2a76107e845abd6ff77232254
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-12-23 15:17:25 +0100

    added Slider to be able to select compression quality

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,8 @@ namespace MauiMedia;
 
 public partial class MainPage : ContentPage
 {
+    private int selectedCompressionQuality;
+
     public MainPage()
     {
         InitializeComponent();
@@ -13,25 +15,34 @@ public partial class MainPage : ContentPage
     private async void OnPickPhotoClicked(object sender, EventArgs e)
     {
         var result = await CrossMedia.Current.PickPhotoAsync();
+        if (result is null) return;
 
         UploadedOrSelectedImage.Source = result?.Path;
-    }
-
-    private async void OnTakePhotoClicked(object sender, EventArgs e)
-    {
-        var options = new StoreCameraMediaOptions { CompressionQuality = 0 };
-        var result = await CrossMedia.Current.TakePhotoAsync(options);
-
-        UploadedOrSelectedImage.Source = result?.Path;
-
-        // CompressionQuality 0: File.Length = 82050 = 82 kB
-
-        // CompressionQuality 100: File.Length = 2398282 = 2398 kb = 2,4 MB
 
         var fileInfo = new FileInfo(result?.Path);
         var fileLength = fileInfo.Length;
 
         FileSizeLabel.Text = $"Image size: {fileLength / 1000} kB";
+    }
+
+    private async void OnTakePhotoClicked(object sender, EventArgs e)
+    {
+        var options = new StoreCameraMediaOptions { CompressionQuality = selectedCompressionQuality };
+        var result = await CrossMedia.Current.TakePhotoAsync(options);
+        if (result is null) return;
+
+        UploadedOrSelectedImage.Source = result?.Path;
+
+        var fileInfo = new FileInfo(result?.Path);
+        var fileLength = fileInfo.Length;
+
+        FileSizeLabel.Text = $"Image size: {fileLength / 1000} kB";
+    }
+
+    private void Slider_DragCompleted(object sender, EventArgs e)
+    {
+        selectedCompressionQuality = (int)((sender as Slider).Value * 100);
+        CompressionLabel.Text = $"Compression: {selectedCompressionQuality}";
     }
 }
 
